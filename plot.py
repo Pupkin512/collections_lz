f320a6010db047c19e85a19e051fa2b30e05e688
Pupkin512 <pupkin123@tutamail.com>
2025-10-23 00:02:01 +0300

    plot.py

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mlt
 import numpy as np
 def f(x):
-    return x**3 + 3*x**2 + 9*x + 7
+    return x**3 + 8*x**2 + x + 6
 x = np.arange(1,30,1)
 y = f(x)
 plt.plot(x,y)
